d33c673aff725654d22b7601479a60bac2fa6ccd
Nogiback <peter.toan.do@gmail.com>
2024-02-09 18:35:21 -0500

    Added sign up functionality

--- controllers/authController.js
+++ controllers/authController.js
@@ -16,10 +16,67 @@ exports.signup_get = function (req, res) {
   res.render("signup", { user: req.user, title: "Nogi Club | Sign Up" });
 };
 
-exports.signup_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET IMPLEMENTED: Signup POST");
-});
+exports.signup_post = [
+  body("firstname", "First Name cannot be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("lastname", "Last Name cannot be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("username", "Username cannot be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("password", "Password cannot be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("confirmPassword", "Confirm password cannot be empty.")
+    .trim()
+    .isLength({ min: 1 })
+    .escape(),
+  body("confirmPassword", "Passwords must match.")
+    .custom((value, { req }) => value === req.body.password)
+    .escape(),
+
+  asyncHandler(async (req, res, next) => {
+    const duplicate = await User.findOne({ username: req.body.username });
+    if (duplicate !== null) {
+      res.render("signup", {
+        user: req.user,
+        errors: [{ msg: "Username already exists." }],
+      });
+    }
+    next();
+  }),
+
+  asyncHandler(async (req, res, next) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty()) {
+      res.render("signup", { user: req.user, errors: errors.array() });
+      return;
+    }
+    bcrypt.hash(req.body.password, 10, async (err, hashedPassword) => {
+      const user = new User({
+        firstname: req.body.firstname,
+        lastname: req.body.lastname,
+        username: req.body.username,
+        password: hashedPassword,
+        roles: ["User"],
+      });
+      await user.save();
+      res.redirect("/");
+    });
+  }),
+];
 
 exports.logout_post = asyncHandler(async (req, res, next) => {
-  res.send("NOT YET IMPLEMENTED: Logout POST");
+  req.logout((err) => {
+    if (err) {
+      return next(err);
+    }
+    res.redirect("/");
+  });
 });

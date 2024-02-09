8c100152b50248d5afd2f5ca01c04ff793905884
Nogiback <peter.toan.do@gmail.com>
2024-02-08 22:27:45 -0500

    Setting up models, controllers and scripts

---
+++ models/Message.js
@@ -0,0 +1,19 @@
+const { TimeAgo } = require("javascript-time-ago");
+const { en } = require("javascript-time-ago/locale/en");
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+TimeAgo.addDefaultLocale(en);
+const timeAgo = new TimeAgo("en-US");
+
+const MessageSchema = new Schema({
+  message: { type: String, maxLength: 750, required: true },
+  timestamp: { type: Date, required: true, default: Date.now },
+  user: { type: Schema.Types.ObjectId, ref: "User", required: true },
+});
+
+MessageSchema.virtual("timestamp").get(function () {
+  return timeAgo.format(this.timestamp);
+});
+
+module.exports = mongoose.model("Message", MessageSchema);
---
+++ models/User.js
@@ -0,0 +1,21 @@
+const mongoose = require("mongoose");
+const Schema = mongoose.Schema;
+
+const UserSchema = new Schema({
+  firstname: { type: String, required: true },
+  lastname: { type: String, required: true },
+  username: { type: String, required: true, unique: true },
+  password: { type: String, required: true },
+  roles: {
+    type: [String],
+    enum: ["User", "Club Member", "Admin"],
+    required: true,
+    default: "User",
+  },
+});
+
+UserSchema.virtual("url").get(function () {
+  return `/user/${this._id}`;
+});
+
+module.exports = mongoose.model("User", UserSchema);
